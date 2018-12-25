54780a24548bb6ef4624466ea5c429f1072eb4a2
abhijit-dey5 <37848197+abhijit-dey5@users.noreply.github.com>
2018-12-25 12:54:57 +0530

    Added changes

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
       LocalTime currentTime = new LocalTime();
 		  System.out.println("The current local time is: " + currentTime);
 	    
-		  System.out.println("New Console Output");
+		  System.out.println("New Console Output is with added changes");
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
